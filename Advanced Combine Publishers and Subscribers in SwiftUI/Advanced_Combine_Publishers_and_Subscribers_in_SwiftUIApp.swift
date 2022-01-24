401cd4522356377e16a6eb8dc836a400b2c96fe2
AlexOliinyk <alex.oliinyk@gmail.com>
2022-01-24 14:21:48 +0200

    Initial Commit

---
+++ Advanced Combine Publishers and Subscribers in SwiftUI/Advanced_Combine_Publishers_and_Subscribers_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_Combine_Publishers_and_Subscribers_in_SwiftUIApp.swift
+//  Advanced Combine Publishers and Subscribers in SwiftUI
+//
+//  Created by Oleksandr Oliinyk on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_Combine_Publishers_and_Subscribers_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Advanced Combine Publishers and Subscribers in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Advanced Combine Publishers and Subscribers in SwiftUI
+//
+//  Created by Oleksandr Oliinyk on 24.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
